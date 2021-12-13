a45f32d641bd1111df41ed795e4e328e98651a62
Chandra Holt <chandra_holt@hotmail.com>
2021-12-13 14:06:30 -0800

    Weather forecast functionality implemented. Forecast appended to HTML and displays on city search.

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,23 @@
-//Global variables
+//Global variables and HTML Elements
 var APIKey = "a97dc7281c9ab7c0ece77353867d800a";
+var tagline = document.createElement("p");
+tagline.innerHTML =
+  "Search for any city's weather! </br> Simply type the name of the city and hit 'SEARCH'. The last five searches will be saved below for your convenience.";
+document.querySelector("#city").appendChild(tagline);
 
-//Dispays current day, date, and time
-// window.setInterval(function () {
-//   document
-//     .getElementById("default-weather")
-//     .html(moment().format("dddd, MMM DD yyyy H:mm:ss"));
-// }, 1000);
-
+//Search button
 document.getElementById("city").addEventListener("submit", handleFormSubmit);
 
-//Finds location
+//Button grabs user input and fetches data from the API
 function handleFormSubmit(event) {
   event.preventDefault();
-
-  //grab the value of the users input
   var userInput = document.getElementById("userInput").value;
-
-  //fetch the data from the api
   locationSearch(userInput);
 }
 
-//TODO: Call on Weather API for local city
+//Calls on Weather API for local city
 function locationSearch(city) {
-
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    APIKey;
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
   fetch(queryURL)
     .then(function (response) {
       if (response.ok) {
@@ -37,69 +26,117 @@ function locationSearch(city) {
         return alert("Error. Please try again.");
       }
     })
-    .then(function(data){
-        findWeather(data);
+    .then(function (data) {
+      findWeather(data);
     });
 }
 
-//TODO: Create a function to find the current weather using API
-function findWeather(coordinates) {
-    var latitutde = coordinates.coord.lat;
-    var longitude = coordinates.coord.lon;
-    var cityName = coordinates.name;
-    var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitutde + "&lon=" + longitude + "&exclude=minutely,hourly&units=imperial&appid=" + APIKey;
+//Finds the current weather using API
+function findWeather(locationData) {
+  var latitutde = locationData.coord.lat;
+  var longitude = locationData.coord.lon;
+  var cityName = locationData.name;
+  var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitutde}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=${APIKey}`;
 
-    fetch(oneCall)
+  fetch(oneCall)
     .then(function (response) {
-        if (response.ok) {
-          return response.json();
-        } else {
-          return alert("Error. Please try again.");
-        }
-      })
-      .then(function(data){
-          //loop through forecast 1-6
-          //pass icon via URL 
-          console.log(data.current, cityName);
-      });
-
+      if (response.ok) {
+        return response.json();
+      } else {
+        return alert("Error. Please try again.");
+      }
+    })
+    .then(function (weatherData) {
+      displayWeather(weatherData);
+      displayForecast(weatherData);
+    });
+  //Displays city name
+  document.getElementById("current-city").innerHTML = cityName;
 }
 
-// //TODO: Create a function to display weather after button click
-// function displayWeather(); {
-//     if (!enteredCity) {
-//         alert("Please enter a valid city name");
-//       } else {
-//         displayWeather();
-//       }
-//     };
+//Displays weather on button click
+function displayWeather(weatherData) {
+  var temperature = weatherData.current.temp;
+  var humidity = weatherData.current.humidity;
+  var windSpeed = weatherData.current.wind_speed;
+  var uvIndex = weatherData.current.uvi;
+  var weatherIcon = weatherData.current.weather[0].icon;
+
+  let locationIcon = document.querySelector(".weather-icon");
+
+  locationIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png">`;
+
+  var weatherBox = document.getElementById("weather-info");
+  weatherBox.innerHTML = `Temperature = ${temperature}°F </br> Humidity = ${humidity}% </br> Wind Speed = ${windSpeed}MPH </br> UV Index = `;
+  var uvBox = document.createElement("span");
+  uvBox.classList.add("badge");
+  if (uvIndex <= 2) {
+    uvBox.classList.add("favorable");
+  } else if (uvIndex >= 6) {
+    uvBox.classList.add("severe");
+  } else {
+    uvBox.classList.add("moderate");
+  }
+  console.log(weatherData);
+  weatherBox.appendChild(uvBox).innerHTML = `${uvIndex}`;
+
+  // Dispays current day, date, and time
+  var currentDate = moment().format("MMMM Do, YYYY");
+  document.getElementById("current-date").innerHTML = currentDate;
+}
+
+//Finds and displays forecast for next week
+function displayForecast(locate) {
+  document.getElementById("forecast-title").style.display = "block";
+  var latitutde = locate.lat;
+  var longitude = locate.lon;
+  var endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitutde}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=${APIKey}`;
+
+  fetch(endpoint)
+    .then(function (response) {
+      if (200 !== response.status) {
+        alert("Error - please try again.");
+        return;
+      }
+
+      response.json().then(function (data) {
+        data.daily.forEach((value, index) => {
+          if (index > 0 && index < 6) {
+            var dayName = new Date(value.dt * 1000).toLocaleDateString("en", {
+              weekday: "short",
+              month: "numeric",
+              day: "numeric",
+            });
+            var forecastCard = document.createElement('div');
+            forecastCard.classList.add('card');
+            forecastCard.classList.add('forecast');
+            forecastCard.classList.add('col-2')
+            var icon = value.weather[0].icon;
+            var temp = value.temp.day.toFixed(0);
+            var wind = value.wind_speed;
+            var humidity = value.humidity;
+            var fday = `<div class="forecast-day">
+                          <h5 class="card-title">${dayName}</h5>
+                          <img src="http://openweathermap.org/img/wn/${icon}.png">
+                          <div class="forecast-day--temp">Temperature: ${temp}°F</div>
+                          <div class="forecast-day--wind">Humidity: ${humidity}%</div>
+                          <div class="forecast-day--humidity">Wind Speed: ${wind}MPH</div>
+                        </div>`;
+            forecastCard.innerHTML = fday;
+            document.getElementById('forecast-container').appendChild(forecastCard);
+
+          }
+        });
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error");
+    });
+}
+
+//TODO: Save last 5 searches on left side of screen, enabling user to re-select those cities
 
 //Fetch the geo data (lat, lon)
 // q = Name of the city
 
 // limit = 5 (optional)
-
-// appid = your custom API key
-
-//Fetch the one call weather data
-
-// lat
-
-// lon
-
-// appid
-
-// units = imperial/metric
-
-// exclude = minutely, hourly
-
-// Print / render the weater data to the page
-
-// From the <form> element, listen to the "submit" OR
-// From the <button> element, listen to the "click"
-
-// Select <input>, get its value, and provide it to the geo API
-
-// Ffrom the <button> container element, listen to the <button> "click"
-
-// Get the city from the button's data attribute
